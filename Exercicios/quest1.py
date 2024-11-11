35a8237e0bbe75c02a1547bf1ab5cc280720b33f
PedroHRC <167247174+PedroCoiado@users.noreply.github.com>
2024-11-11 08:30:58 -0300

    quest1.py

--- Exercicios/quest1.py
+++ Exercicios/quest1.py
@@ -4,6 +4,6 @@ num1 = int(input("Digite o primeiro número"))
 num2 = int(input("Digite o segundo número"))
 
 if num1 > num2:
-  print(f"O maior número é o num1: {num1}")
+  print("O maior número é o num1: ", num1)
 else:
-  print(f"O maior número é o num2: {num2}")
+  print("O maior número é o num2: ", num2)
