2ff2ad29c86552a713ae61cb4bb14d5cc150ae57
PedroHRC <167247174+PedroCoiado@users.noreply.github.com>
2024-11-11 08:49:13 -0300

    quest3.py

--- Exercicios/quest3.py
+++ Exercicios/quest3.py
@@ -12,3 +12,14 @@ elif sexo == "M" or sexo == "m":
     print("Seu sexo é Masculino")
 else:
     print("Sexo inválido")
+
+# O comando lower() converte as letras para minúsculas
+# O comando upper() converte as letras para maiúsculas
+
+# sexo = sexo.lower()
+# if sexo == "f":
+#     print("Feminino")
+# elif sexo == "m":
+#     print("Masculino")
+# else:
+#     print("Sexo inválido")
