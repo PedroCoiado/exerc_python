fa55a097dd2e31a2170b5d3d886971e804194b45
PedroHRC <167247174+PedroCoiado@users.noreply.github.com>
2024-11-11 08:37:18 -0300

    quest3.py

--- Exercicios/quest3.py
+++ Exercicios/quest3.py
@@ -1,14 +1,14 @@
 # Faça um Programa que verifique se uma letra digitada é "F" ou "M". 
 # Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
 
-sigla = input("Digite a sigla do seu gênero (F ou M):") 
+sexo = input("Digite a sigla do seu gênero (F ou M):") 
 
 # F = "Feminino"
 # M = "Masculino"
 
-if sigla == "F":
-    print("seu genero é Feminino")
-elif sigla == "M":
-    print("Seu genero é Masculino")
+if sexo == "F" or sexo == "f":
+    print("seu sexo é Feminino")
+elif sexo == "M" or sexo == "m":
+    print("Seu sexo é Masculino")
 else:
     print("Sexo inválido")
